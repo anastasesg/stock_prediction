a2d4bf5778b6b29ec1c44a70dc2f4630c4a7af13
anastasesg <anastasesgiannikopoulos@gmail.com>
2021-03-01 15:48:31 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,85 +1,151 @@
-## Miscelaneous libraries
-from types import BuiltinFunctionType
-from tensorflow.keras import callbacks
-from tqdm import tqdm
-import numpy as np
 import math
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-## Libraries for the model creation
-import tensorflow
-import keras
-from keras.callbacks import ModelCheckpoint
-from keras.models import Sequential
-from keras.layers import LSTM
-from keras.layers import Dense
-from keras.models import load_model
-
-## Libraries for data collection, presentation and processing
+import numpy as np
+import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
-
+from datetime import date, datetime
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+import tensorflow as tf
+from keras.models import load_model
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM
+from keras.callbacks import ModelCheckpoint
+
+import getopt, sys
+
+tf.random.set_seed(7)
+np.random.seed(7)
 
 
-# Define functions
-def data_collection():
-    data = yf.download(TICKER)
-    data = data[[CATEGORY]]
-    return data
+class Predictor:
+    def __init__(self, percent, window_size, ticker, category, par_dir, score_name):
+        self.__window_size = window_size
+        self.ticker = ticker
+        self.category = category
+        self.scaler = MinMaxScaler(feature_range=(0, 1))
+        self.model_dir = par_dir
+        self.model = Sequential()
+        self.score = score_name
+        self.load_data(percent)
+        self.process_data()
 
+    def create_features(self, data):
+        x, y = [], []
+        for i in range(self.__window_size, len(data)):
+            x.append(data[i - self.__window_size:i, 0])
+            y.append([data[i:i, 0]])
+        return np.array(x), np.array(y)
 
-def train_test_split(data, percent):
-    scaled_data = SCALER.fit_transform(data)
-    train_size = math.ceil(len(scaled_data) * percent)
-    return scaled_data[:train_size], scaled_data[train_size:]
+    def load_data(self, percent):
+        print('Loading Data...')
+        self.data = yf.download(self.ticker)
+        self.data = self.data[[self.category]]
+        self.train_size = math.ceil(len(self.data) * percent)
+        self.train, self.valid = self.data[:self.train_size], self.data[self.train_size:]
 
+    def process_data(self):
+        dataset = self.data.values
+        dataset = dataset.astype('float32')
 
-def feature_creation(data):
-    x, y = [], []
+        scaled_data = self.scaler.fit_transform(dataset)
 
-    for i in range(WINDOW_SIZE, len(data)):
-        x.append(data[i - WINDOW_SIZE:i, 0])
-        y.append([data[i:i, 0]])
+        train, test = scaled_data[:self.train_size, :], scaled_data[self.train_size - self.__window_size:, :]
+
+        self.x_train, self.y_train = self.create_features(train)
+        self.x_test, self.y_test = self.create_features(test)
+
+        self.x_train = np.reshape(self.x_train, (self.x_train.shape[0], self.x_train.shape[1], 1))
+        self.x_test = np.reshape(self.x_test, (self.x_test.shape[0], self.x_test.shape[1], 1))
+
+    def plot_data(self):
+        plt.figure()
+        plt.title('Model for {}'.format(self.ticker))
+        plt.xlabel('Date')
+        plt.ylabel('{} Price USD ($)'.format(self.category))
+        plt.plot(self.train[self.category])
+        plt.plot(self.valid[[self.category, 'Predictions']])
+        plt.legend(['Train', 'Valid', 'Predictions'], loc='lower right')
+        plt.show()
+
+    def build_model(self, optimizer, loss, save, epochs, batch_size):
+        self.model.add(LSTM(300, return_sequences=True, input_shape=(self.x_train.shape[1], 1)))
+        self.model.add(LSTM(250, return_sequences=True))
+        self.model.add(LSTM(200, return_sequences=False))
+        self.model.add(Dense(50))
+        self.model.add(Dense(1))
+        self.model.compile(optimizer=optimizer, loss=loss)
+
+        if save:
+            filepath = os.path.join(self.model_dir, 'model_epoch_{epoch:02d}.hdf5')
+            checkpoint = ModelCheckpoint(
+                filepath=filepath,
+                monitor='val_loss',
+                verbose=True,
+                save_best_only=True,
+                mode ='min'
+            )
+            self.model.fit(
+                self.x_train,
+                self.y_train,
+                epochs=epochs,
+                batch_size=batch_size,
+                callbacks=[checkpoint],
+                validation_data=(self.x_test, self.y_test)
+            )
+        else:
+            self.model.fit(self.x_train, self.y_train, epochs=epochs, batch_size=batch_size)
+
+    def model_selection(self):
+        score_list, model_list = [], []
+        filelength = len(os.listdir(self.model_dir))
+        for num, file in enumerate(os.listdir(self.model_dir)):
+            filepath = os.path.join(self.model_dir, file)
+            print('Checking file {0}/{1} : {2}'.format(num + 1, filelength, file), end="\r", flush=True)
+            
+            self.model = load_model(filepath)
+            predict = self.scaler.inverse_transform(self.model.predict(self.x_test))
+
+            model_list.append(file)
+            score_list.append(self.score(self.y_test, predict))
+        print()
+        score_list, model_list = zip(*sorted(zip(score_list, model_list), reverse=True))
+        return os.path.join(self.model_dir, model_list[0])
+
+    def get_model(self, optimizer='adam', loss='mean_squared_error', save=False, epochs=10, batch_size=10):
+        if len(os.listdir(self.model_dir)) == 0 or save:
+            self.build_model(optimizer, loss, save, epochs, batch_size)
+        # model_file = self.model_selection()
+        # print(model_file)
+        self.model = load_model(os.path.join(self.model_dir, os.listdir(self.model_dir)[-1]))
     
-    x, y = np.array(x), np.array(y)
-    x = np.reshape(x, (x.shape[0], x.shape[1], 1))
+    def predict_tomorrow(self):
+        last_window = np.array([self.scaler.transform(self.data[-self.__window_size:].values)])
+        last_window = np.reshape(last_window, (last_window.shape[0], last_window.shape[1], 1))
 
-    return x, y
-
-
-def build_model(train_x, train_y, test_x, test_y):
-    MODEL.add(LSTM(300, return_sequences=True, input_shape=(train_x.shape[1], 1)))
-    MODEL.add(LSTM(250, return_sequences=False))
-    MODEL.add(Dense(1))
-    MODEL.compile(optimizer='adam', loss='mean_squared_error')
-
-    filename = os.path.join(PARENT_DIRECTORY, 'model_epoch_{epoch:02d}.hdf5')
-    checkpoint = ModelCheckpoint(filepath=filename, monitor='val_loss', verbose=True, save_best_only=False, mode ='min')
-    MODEL.fit(train_x, train_y, epochs = 10, batch_size = 100, callbacks = [checkpoint], validation_data = (test_x, test_y))
+        return self.scaler.inverse_transform(self.model.predict(last_window))
 
 
-def select_model():
-    temporary_model
-
-# Constants
-TICKER = 'TSLA'
-CATEGORY = 'Adj Close'
-WINDOW_SIZE = 60
-PARENT_DIRECTORY = 'saved_models'
-
-SCALER = MinMaxScaler()
-MODEL = Sequential()
-
-# Data collection
-data = data_collection()
-
-# Data processing
-train, test = train_test_split(data, 0.9)
-train_x, train_y = feature_creation(train)
-test_x, test_y = feature_creation(test)
-
-# Build/trian model
-build_model(train_x, train_y, test_x, test_y)
+if __name__ == '__main__':
+    predictor = Predictor(
+        percent=0.95,
+        window_size=30,
+        ticker='HLTOY',
+        category='Adj Close',
+        par_dir='saved_models',
+        score_name=mean_squared_error
+    )
+    predictor.get_model(
+        save=False,
+        # epochs=1,
+        # batch_size=30
+    )
+    prediction = predictor.scaler.inverse_transform(predictor.model.predict(predictor.x_test))
+    predictor.valid['Predictions'] = prediction
+    predictor.plot_data()
+    print(predictor.valid)
+    print(predictor.predict_tomorrow())
